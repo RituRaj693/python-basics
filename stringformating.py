b3122e19ba2b74891f0c3fb8b51eed5793940061
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-01-23 04:30:53 +0000

    Add files via upload

---
+++ stringformating.py
@@ -0,0 +1,5 @@
+name = 'ritu'
+age = 24
+print(f'hello {name} your age is {age +2}') #we can add and substract in this also ####python 3.6
+#python 3
+print('hello {} your age is {}'.format(name, age))
