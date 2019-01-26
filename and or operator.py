47ee9f67c061ff1f4fa8db1bf5efa7a14235b27d
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-01-26 14:40:15 +0000

    Add files via upload

---
+++ and or operator.py
@@ -0,0 +1,11 @@
+age = 23
+name ='ritu'
+if age==23 or name=='rituraj':
+#if age==23 and name=='ritu':
+    print('condition true')
+else:
+    print ('condition false')
+
+
+    ######### in or condition any one condition is true then it run.
+    ########in and operator all condition must be true than the condition is true
