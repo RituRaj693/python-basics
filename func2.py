f57e6248ccadaa731eafa224c7b90dc0e44ee4d7
RituRaj693 <45709450+RituRaj693@users.noreply.github.com>
2019-02-20 12:56:46 +0000

    Add files via upload

---
+++ func2.py
@@ -0,0 +1,6 @@
+def ritu_raj(aaaa,bbbb):
+    return(aaaa + bbbb)
+a = int(input("enter  first the numbar :"))
+b = int(input("enter the second number :"))
+#total = ritu_raj(a,b)########################################### without storing into any variable 
+print(ritu_raj(a,b))
---
+++ funcgreater.py
@@ -0,0 +1,10 @@
+def greater_number(num1,num2):
+    if (num1>num2):
+        return(num1)
+    else:
+        return(num2)
+num1 = int(input('enter the first number :'))
+num2 = int(input('enter the second the number :'))
+bigger = (greater_number(num1,num2))
+print(f'{bigger :} is  greater')##############################using f string function
+
